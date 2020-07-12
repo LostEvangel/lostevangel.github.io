bb55cff34b884ee89adf2969c4e85f8651427bf7
keinz <553636890@qq.com>
2020-07-12 20:29:01 +0800

    add next

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+    {
+        name: "狂おしいほど僕には美しい",
+        artist: 'まじ娘',
+        url: 'http://39.106.54.239:8888/majiko.mp3',
+        cover: 'https://p2.music.126.net/qdaktuZnG_rPFnlSzXa0XA==/109951163811354022.jpg?param=130y130',
+      }
+    ]
+});
